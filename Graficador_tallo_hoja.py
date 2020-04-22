859d1c081da15eab1377d56d2a4e1cadda9e3528
unknown <pragmaticalli-dev>
2020-04-22 14:08:45 -0300

    bug fixed for in line

--- Graficador_tallo_hoja.py
+++ Graficador_tallo_hoja.py
@@ -57,7 +57,7 @@ def resolverDiagrama(t,conj):
 
 
 def mostrarDiagrama(dataraw,tallos):
-    aux=[""*len(tallos)]
+    aux=["" for i in range(len(tallos))]
     for x in range(len(tallos)):
         for i in dataraw:
             if(i[0]==tallos[x]):
